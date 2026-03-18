20ba4dc21b4144f37d06330c37605cc984ef3ea6
Priya Jagad <priya.jagad007@gmail.com>
2026-03-18 18:48:20 +0530

    Player and Buildings Setup, Player Jump Movement

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 8f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0.0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
